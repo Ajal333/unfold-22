f79021b10a1d29e105493e75e37117ed6ccf5a24
Nandan-unni <asnqln@gmail.com>
2022-08-28 02:05:28 +0530

    fix: in between

---
+++ app/presentation/components/PitchCard/index.tsx
@@ -0,0 +1,22 @@
+import { Collapse, Text } from "@nextui-org/react";
+import React from "react";
+
+const PitchCard = () => {
+  return (
+    <Collapse
+      title={
+        <div>
+          <Text>ajapoosatta@gmail.com</Text>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </Text>
+        </div>
+      }
+    ></Collapse>
+  );
+};
+
+export default PitchCard;
